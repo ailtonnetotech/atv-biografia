0ef145f67daac10edcdc5c79b9ccaef42e0341c8
Ailton Neto <ailtonneto.pessoal@gmail.com>
2023-11-30 18:08:30 -0400

    enviando novas modificações

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function curiosidade1() {
+    alert('Você sabia que... adadsd sd sd s d sd sd s d')
+}
+
+function curiosidadeNascimentoAlbert() {
+    alert('Aqui está uma curiosidade sobre o nascimento de Albert Einsten.')
+}
+function curiosidadeProducaoCientificaAlbert() {
+    alert('Vc deseja conhecer sobre as produção ciêncitifica de Albert Einsten.')
+}
+function curiosidadeLegado() {
+    alert('Você sabia qu')
+}
